fc7c27f25f85061a860f9b5d5ba80321b3a4d35a
kstatz12 <karl.statz@gmail.com>
2022-01-24 18:38:34 -0600

    hard reset

--- examples/cpp1/weekeight/main.cpp
+++
@@ -1,5 +0,0 @@
-#include <iostream>
-#include <map>
-#include <vector>
-
-int main() { std::map<int, int> m = {{1, 1}, {2, 2}, {3, 3}}; }
--- examples/cpp1/weeknine/inline.cpp
+++
@@ -1,3 +0,0 @@
-int foo(int x, int y) { return x + y; }
-
-int main() { int sum = foo(1, 2); }
